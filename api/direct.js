b7f808597e04860a60d9e93f745fe7a090d66b29
laont <stanislav@laont.me>
2020-06-23 01:28:07 +0300

    Move from serverfull to serverless Update domain

---
+++ api/direct.js
@@ -0,0 +1,22 @@
+const megajs = require('megajs');
+const filetype = require('file-type');
+
+module.exports = async (req, res) => {
+  let link = decodeURIComponent(req.params.link);
+  let file = megajs.file(link);
+  const options = {
+    maxConnections: 10,
+    initialChunkSize: 64000,
+    chunkSizeIncrement: 64000,
+    maxChunkSize: 1000000,
+    returnCiphertext: false
+  }
+
+  file.loadAttributes((err, file) => {
+    file.download(options, (err, data) => {
+      let ft = await filetype.fromBuffer(data);
+      res.setHeader('Content-Type', ft.mime);
+      res.send(data);
+    });
+  });
+}
