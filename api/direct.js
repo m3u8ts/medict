98b7359ec96f808976e4d66cbd7a5767eec9e475
laont <stanislav@laont.me>
2020-06-23 12:56:23 +0300

    Try to remove content-type

--- api/direct.js
+++ api/direct.js
@@ -14,8 +14,8 @@ module.exports = async (req, res) => {
 
   file.loadAttributes((err, file) => {
     file.download(options, async (err, data) => {
-      let ft = await filetype.fromBuffer(data);
-      res.setHeader('Content-Type', ft.mime);
+      //let ft = await filetype.fromBuffer(data);
+      //res.setHeader('Content-Type', ft.mime);
       res.send(data);
     });
   });
